f69d3d92577c5560f2fc4322905ec34fd87d806f
Sittha Darapisut <katoretv@gmail.com>
2023-11-11 21:24:14 +0700

    I want to see error

--- backend/src/route/register.py
+++ backend/src/route/register.py
@@ -10,110 +10,110 @@ import json
 import pyotp
 
 def main():
-    try:
-        data = request.get_json()
-        fn = data["firstname"]
-        ln = data["lastname"]
-        sid = data["sid"]
-        un = data["username"]
-        password = data["password"]
-        ip = request.remote_addr
-        msg, err = valid_sid(sid)
-        if err:
-            return msg
+    # try:
+    data = request.get_json()
+    fn = data["firstname"]
+    ln = data["lastname"]
+    sid = data["sid"]
+    un = data["username"]
+    password = data["password"]
+    ip = request.remote_addr
+    msg, err = valid_sid(sid)
+    if err:
+        return msg
 
-        result, err = sql.sqsel("users", ["sid"], f"sid='{sid}'")
-        if err:
-            return result
+    result, err = sql.sqsel("users", ["sid"], f"sid='{sid}'")
+    if err:
+        return result
 
-        if len(result) != 0:
-            return errmaker(400, "user already exist")
+    if len(result) != 0:
+        return errmaker(400, "user already exist")
 
-        default_working_hour = []
-        for i in range(7):
-            x = {
-                "day": i+1,
-                "hours": [
-                    [8, 0],
-                    [16, 0]
-                ],
-                "busy_hours": [
-                    [
-                        [12, 0],
-                        [13, 0]
-                    ]
-                ]
-            }
-            default_working_hour.append(x)
-
-        sc = encrypt_string(f'{randrange(1, 10**10):010}'+sid, "md5")
-
-        salt = ''.join(choice(ascii_letters+digits) for i in range(20))
-        password = f'{encrypt_string(password+salt, "sha256")}${salt}'
-
-        token = {ip: encrypt_string(''.join(choice(ascii_letters+digits) for i in range(20))+sid+ip, "sha256")}
-
-        k = pyotp.random_base32()
-
-        ke = [
-                "sid",
-                "username",
-                "firstname",
-                "lastname",
-                "password",
-                "working_hour",
-                "subjects",
-                "token",
-                "secret_code",
-                "authkey"
-                ]
-        va = [  
-                f'{sid}',
-                f'{un}',
-                f'{fn}',
-                f'{ln}',
-                f'{password}',
-                f'{json.dumps(default_working_hour)}',
-                f'{{}}',
-                f'{json.dumps(token)}',
-                f'{sc}',
-                f'{k}'
-                ]
-        result, err = sql.sqadd("users", ke, va)
-        if err:
-            return result
-
-        result, err = sql.sqadd("logs", ["ip", "info"], [ip, f'add user sid={sid}'])
-        if err:
-            return result
-
-        c = [
-            "id INT NOT NULL AUTO_INCREMENT",
-            "event_title VARCHAR(256) NOT NULL",
-            "event_des VARCHAR(256) NOT NULL",
-            "event_start DATETIME NOT NULL",
-            "event_end DATETIME NOT NULL",
-            "event_color VARCHAR(7) NOT NULL",
-            "PRIMARY KEY (id)"
-        ]
-        result, err = sql.sqcre(f"{sid}_events", c)
-        if err:
-            return result
-        
-        totp_auth = pyotp.totp.TOTP(k).provisioning_uri( 
-            name=sid, 
-            issuer_name='CUCalen'
-        )
-
+    default_working_hour = []
+    for i in range(7):
         x = {
-            "status_code": 200,
-            "success": True,
-            "message": "Add account success",
-            "data": {
-                "token": token[ip],
-                "qr": mb64qr(totp_auth)
-            }
+            "day": i+1,
+            "hours": [
+                [8, 0],
+                [16, 0]
+            ],
+            "busy_hours": [
+                [
+                    [12, 0],
+                    [13, 0]
+                ]
+            ]
         }
-        return jsonify(x)
-    except:
-        return errmaker(400, "Bad Request")
+        default_working_hour.append(x)
+
+    sc = encrypt_string(f'{randrange(1, 10**10):010}'+sid, "md5")
+
+    salt = ''.join(choice(ascii_letters+digits) for i in range(20))
+    password = f'{encrypt_string(password+salt, "sha256")}${salt}'
+
+    token = {ip: encrypt_string(''.join(choice(ascii_letters+digits) for i in range(20))+sid+ip, "sha256")}
+
+    k = pyotp.random_base32()
+
+    ke = [
+            "sid",
+            "username",
+            "firstname",
+            "lastname",
+            "password",
+            "working_hour",
+            "subjects",
+            "token",
+            "secret_code",
+            "authkey"
+            ]
+    va = [  
+            f'{sid}',
+            f'{un}',
+            f'{fn}',
+            f'{ln}',
+            f'{password}',
+            f'{json.dumps(default_working_hour)}',
+            f'{{}}',
+            f'{json.dumps(token)}',
+            f'{sc}',
+            f'{k}'
+            ]
+    result, err = sql.sqadd("users", ke, va)
+    if err:
+        return result
+
+    result, err = sql.sqadd("logs", ["ip", "info"], [ip, f'add user sid={sid}'])
+    if err:
+        return result
+
+    c = [
+        "id INT NOT NULL AUTO_INCREMENT",
+        "event_title VARCHAR(256) NOT NULL",
+        "event_des VARCHAR(256) NOT NULL",
+        "event_start DATETIME NOT NULL",
+        "event_end DATETIME NOT NULL",
+        "event_color VARCHAR(7) NOT NULL",
+        "PRIMARY KEY (id)"
+    ]
+    result, err = sql.sqcre(f"{sid}_events", c)
+    if err:
+        return result
+    
+    totp_auth = pyotp.totp.TOTP(k).provisioning_uri( 
+        name=sid, 
+        issuer_name='CUCalen'
+    )
+
+    x = {
+        "status_code": 200,
+        "success": True,
+        "message": "Add account success",
+        "data": {
+            "token": token[ip],
+            "qr": mb64qr(totp_auth)
+        }
+    }
+    return jsonify(x)
+    # except:
+    #     return errmaker(400, "Bad Request")
