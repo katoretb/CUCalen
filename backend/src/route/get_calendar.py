b6386c8f53123cb904ec1f2dda8b5b598c8f25cc
Penek Suksuda <admin@peneksuksuda.com>
2023-11-17 01:04:18 +0700

    Implemented get_event.py into get_calendar.py

--- backend/src/route/get_calendar.py
+++ backend/src/route/get_calendar.py
@@ -14,13 +14,34 @@ def main():
         result, err = valid_sc(sc)
         if err:
             return result
+
+        sid = result[0][1]
         
         resul, err = sql.sqadd("logs", ["ip", "info"], [ip, f'get user secret_code={sc} calendar'])
         if err:
             return resul
+
+        result_event, err = sql.sqsel(f"{sid}_events", ["id", "event_title", "event_des", "event_start", "event_end", "event_color"])
+        if err:
+            return result_event
+
+        event_list = []
+
+        if len(result_event) > 0:
+            for i in result_event:
+                e = {
+                    "event_id": int(i[0]),
+                    "event_title": i[1],
+                    "event_des": i[2],
+                    "event_date": [
+                        str(i[3]).replace(" ", "T"),
+                        str(i[4]).replace(" ", "T")
+                    ],
+                    "event_color": i[5]
+                }
+                event_list.append(e)
         
         sub_list = []
-        event_list = []
         # get_calendar operations will start here
 
         x = {
