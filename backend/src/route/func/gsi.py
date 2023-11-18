fb8aed8c4035f54955454f185d2ed2a8b219d904
Sittha Darapisut <katoretv@gmail.com>
2023-11-18 18:31:31 +0700

    reformat gsi datetime

--- backend/src/route/func/gsi.py
+++ backend/src/route/func/gsi.py
@@ -113,13 +113,13 @@ def gsi(c: str, y: int, se: int, sp='S'): #<====================================
     try:
         temp = table[3]['tr'][0]['td'][0]['nobr'][0]['font'][1]['_value'].split(" ")
         temp2 = [f"{x}:00" for x in temp[4].split("-")]
-        tempmid = [f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[0]}', f'{temp[0]}-{m[temp[1]]}-{int(temp[2])-543}T{temp2[1]}']
+        tempmid = [f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[0]}', f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[1]}']
     except:
         tempmid = ""
     try:
         temp = table[3]['tr'][0]['td'][0]['nobr'][0]['font'][3]['_value'].split(" ")
         temp2 = [f"{x}:00" for x in temp[4].split("-")]
-        tempfin = [f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[0]}', f'{temp[0]}-{m[temp[1]]}-{int(temp[2])-543}T{temp2[1]}']
+        tempfin = [f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[0]}', f'{int(temp[2])-543}-{m[temp[1]]}-{temp[0]}T{temp2[1]}']
     except:
         tempfin = ""
     temp = {
