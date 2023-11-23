ea46d8765a9582bb9260e25b1b6c89b4555eefec
Penek Suksuda <admin@peneksuksuda.com>
2023-11-23 07:46:26 +0700

    Update language in register.py

--- backend/src/route/register.py
+++ backend/src/route/register.py
@@ -29,7 +29,7 @@ def main():
             return result
 
         if len(result) != 0:
-            return errmaker(400, "The user already exists")
+            return errmaker(400, "User already exists")
 
         default_working_hour = []
         for i in range(7):
